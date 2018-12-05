8b31cea8355bd02286a17fc6116e9a367de7c9df
oguzhalit <oguzhalit.sak@gmail.com>
2018-12-05 14:30:53 +0300

    djangoman

---
+++ python-intro.py
@@ -0,0 +1,9 @@
+
+def hi(name):
+	if name == 'Osman':
+		print("osman sen bir hayinsin kardeşim")
+	elif name == 'oguz':
+		print("Adamsın reiiiiiiiiiiis")
+	else: 
+		print("sen kimsin mk")
+hi("Osman")
---
+++ python-pintro.py
@@ -0,0 +1,7 @@
+def hi(name):
+	print('Merhaba' + name + '!')
+
+hainler = ['Hitler','Osman','Kütük','Mikimouse']
+for name in hainler:
+	hi(name)
+	print('SIRADAKI HAIN')
